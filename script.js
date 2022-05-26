6815b52de5fef2f1a333f146a66d12ecb7cecc40
BCham16 <bchamberlain.3618@gmail.com>
2022-05-26 09:59:34 -0400

    updated HTML, started js

---
+++ script.js
@@ -0,0 +1,22 @@
+
+/*
+console.dir(document);
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.forms);
+console.log(document.links);
+console.log(document.images);
+*/
+
+// Variable definitions using GETELEMENTBYID
+var dayOfWeek = document.getElementById('dayOfWeek');
+var monthOfYear = document.getElementById('monthOfYear');
+var dateOfMonth = document.getElementById('dateOfMonth');
+var year = document.getElementById('year');
+
+// console.log(dayOfWeek);
+// dayOfWeek.textContent = "hello";
