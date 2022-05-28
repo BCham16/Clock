795c61a92512d99c6c89d53f32217b0e362383f6
BCham16 <bchamberlain.3618@gmail.com>
2022-05-27 22:37:33 -0400

    code update / bug fix

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 
-// Variable definitions of HTML Elments using GETELEMENTBYID
-date = document.getElementById('date')
-hours = document.getElementById('hours');
-minutes = document.getElementById('minutes');
-seconds = document.getElementById('seconds');
+// Variable definitions of HTML Elments
+const date = document.getElementById('date')
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
 
-//Arrays for displaying proper text
-weekDay = new Array(
+//Arrays for displaying proper Day & Month text
+const weekDay = new Array(
   'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
   );
-month = new Array(
+const month = new Array(
     'January', 'February', 'March', 'April', 'May', 'June', 'July', 
     'August', 'September', 'October', 'November', 'December'
   );
@@ -17,17 +17,17 @@ month = new Array(
 
 function showTime(){
 //Get Current Date
-today = new Date();
+let today = new Date();
 
 //Numeric value of .getDay() as an index of the weekDay array
-dayToPrint = weekDay[today.getDay()];
+let dayToPrint = weekDay[today.getDay()];
 
 //Numeric value of .getMonth() as an index of month array
-monthToPrint = month[today.getMonth()];
+let monthToPrint = month[today.getMonth()];
 
 //set descriptor after numeric date value
-descriptor = new String;
-temp = today.getDate();
+let descriptor = new String;
+let temp = today.getDate();
 if (temp == 1){descriptor = 'st'}
 else if (temp == 2){descriptor = 'nd'}
 else if (temp == 3){descriptor = 'rd'}
@@ -35,28 +35,27 @@ else if (temp > 20 && temp % 10 == 1){descriptor = 'st'}
 else if (temp > 20 && temp % 10 == 2){descriptor = 'nd'}
 else if (temp > 20 && temp % 10 == 3){descriptor = 'rd'}
 else {descriptor = 'th'}
-dayOfMonth = String(temp) + String(descriptor);
-console.log(dayOfMonth)
+let dayOfMonth = String(temp) + String(descriptor);
 
 //Assigning a variable to year for consistency
-yearToPrint = today.getFullYear();
+let yearToPrint = today.getFullYear();
 
-dateToPrint = dayToPrint + ', ' + monthToPrint + ' ' + dayOfMonth + ' ' + yearToPrint;
+let dateToPrint = dayToPrint + ', ' + monthToPrint + ' ' + dayOfMonth + ' ' + yearToPrint;
 date.textContent = dateToPrint; 
 
 //Set Time Info
-hh = today.getHours();
-
-// Code to covert to 12h format
+let hh = today.getHours();
+// Code to covert to 12h format (not including AM/PM signifier)
 /*
 if (hh > 12){hh -= 12}
 else if (hh == 0){hh = 12}
 */
-
 if (hh < 10){hh = '0' + hh};
-mm = today.getMinutes();
+
+let mm = today.getMinutes();
 if (mm < 10){mm = '0' + mm};
-ss = today.getSeconds();
+
+let ss = today.getSeconds();
 if (ss < 10){ss = '0' + ss};
 
 //update textContent with time information
